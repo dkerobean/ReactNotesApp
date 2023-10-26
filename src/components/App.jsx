d5c9f42aa4feb3f2701af9dbadf1613fdc94569e
Dickson <dkerobean@gmail.com>
2023-10-26 09:41:11 +0000

    pages and components add

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NotesListPage from '../pages/NotesListPage';
+
+function App() {
+
+    return (
+        <>
+            <h1> This is my first React App</h1>
+            <NotesListPage />
+        </>
+    )
+}
+
+export default App;
---
+++ src/pages/NotesListPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function NotesListPage() {
+    return (
+        <>
+            <p>Notes Body</p>
+        </>
+    )
+}
+
+export default NotesListPage;
