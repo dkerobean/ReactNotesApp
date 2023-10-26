850b64b1065b38219915fe5b2b5c5a60b61bd470
Dickson <dkerobean@gmail.com>
2023-10-26 11:47:47 +0000

    react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NotesListPage from '../pages/NotesListPage';
+import NotePage from '../pages/NotePage';
 
 function App() {
-
-    return (
-        <>
-            <h1> This is my first React App</h1>
-            <NotesListPage />
-        </>
-    )
+  return (
+    <Router>
+      <>
+        <h1>This is my first React App</h1>
+        <Switch>
+          <Route path='/' exact component={NotesListPage} />
+          <Route path='/notes' component={NotePage} />
+        </Switch>
+      </>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/NotePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function NotePage() {
+    return (
+        <>
+            <h3> This is a single note page</h3>
+        </>
+    )
+}
+
+export default NotePage;
